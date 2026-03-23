41dd2bd4a9a6a8f41f975edc6c5de7da38a6d2a4
abirami20051019 <abirami20051019@gmail.com>
2026-03-23 23:24:57 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,11 @@
+public class even{
+    public static void main(String[] args){
+        int a=20;
+        if(a%2==0){
+            System.out.print("even");
+        }
+        else{
+            System.out.print("odd");
+    }
+    }
+}
