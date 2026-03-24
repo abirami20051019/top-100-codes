272aff2d4842805d3c95fea5fda88c6db2e2338f
abirami20051019 <abirami20051019@gmail.com>
2026-03-24 22:41:05 +0530

    Add files via upload

---
+++ greaterno.java
@@ -0,0 +1,14 @@
+public class greaterno{
+    
+   public static void main(String[] args){
+    int num1=10;
+    int num2=20;
+    if(num1==num2){
+        System.out.print("equal");
+    }
+    else{
+        String result=num1>num2?"num1 is big":"num2 is big";
+        System.out.print(result);
+    }
+   }
+}
